cb0432ffb3ec66600aa9861748ff83832616480d
osfprieto <osfprieto@gmail.com>
2021-05-10 17:47:54 -0700

    argv

--- js/reddit.js
+++ js/reddit.js
@@ -1,16 +1,18 @@
 const fetch = require("node-fetch");
 
 async function getRedditTokens(url) {
-    var promesa = fetch(url);
-    var respuesta = await promesa;
-    var myJson = await respuesta.json();
-    var posts = myJson.data.children;
-    var mapa = {};
+    const promesa = fetch(url);
+    const respuesta = await promesa;
+    const myJson = await respuesta.json();
+    const posts = myJson.data.children;
+    const mapa = {};
     posts.forEach(post => post.data.title.split(' ').forEach(token => {var cadena = token.trim().toLowerCase(); mapa[cadena] = mapa[cadena] ? (mapa[cadena] + 1) : 1}));
-    var arreglo = [];
+    const arreglo = [];
     Object.keys(mapa).forEach(token => arreglo.push({palabra: token, conteo: mapa[token]}));
     arreglo.sort((objeto1, objeto2) => objeto2.conteo - objeto1.conteo);
     console.log(arreglo);
 }
 
-getRedditTokens('https://www.reddit.com/r/AskReddit/hot.json');
+// '/r/AskRedit'
+const urlParam = process.argv[2];
+getRedditTokens(`https://www.reddit.com${urlParam ? urlParam :  ''}/hot.json`);
