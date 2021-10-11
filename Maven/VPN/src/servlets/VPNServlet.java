6dc981014fd562ba336bcf772a17a0b44ef943df
Omar Prieto <omprieto@microsoft.com>
2021-10-10 19:19:43 -0700

    Trying to make things boot.

--- Maven/VPN/src/servlets/VPNServlet.java
+++ Maven/VPN/src/servlets/VPNServlet.java
@@ -1,5 +1,16 @@
 package servlets;
 
-public class VPNServlet {
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+public class VPNServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Set response content type
+		response.setContentType("text/html");
+		
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1>kmas</h1>");
+	}
 }
